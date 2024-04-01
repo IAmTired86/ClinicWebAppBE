ddbeba17ee2c9a98c0cd745fc481a8f784684905
IAmTired86 <minhltt.bi12-290@st.usth.edu.vn>
2024-04-01 14:39:08 +0700

    Adding doctor and patient class

---
+++ Models/Doctor.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Specialty { get; set; }
+        public string PhoneNumber { get; set; }
+        
+    }
+}
